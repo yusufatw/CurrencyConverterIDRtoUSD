17a244891fcec63c525de0dfbc9efc0b7100d2d9
yusufatw <yusufatw@gmail.com>
2018-09-11 13:08:21 +0700

    Fixing bug when input is empty, using if statement

--- app/src/main/java/id/cathode/currencyconverterrp_usd/MainActivity.java
+++ app/src/main/java/id/cathode/currencyconverterrp_usd/MainActivity.java
@@ -10,12 +10,17 @@ public class MainActivity extends AppCompatActivity {
 
     public void convertToUSD(View view){
         EditText ammountEditText = findViewById(R.id.ammountEditText);
-        int parseInt = Integer.parseInt(ammountEditText.getText().toString());
 
-        Double convertResult = parseInt * 0.000069;
+        if (ammountEditText.getText().toString().isEmpty()){
+            Toast.makeText(getApplicationContext(),"Please input a number",Toast.LENGTH_SHORT).show();
+        } else {
+            int parseInt = Integer.parseInt(ammountEditText.getText().toString());
+            Double convertResult = parseInt * 0.000069;
 
-        Toast.makeText(getApplicationContext(), (String.format("USD %.2f", convertResult)), Toast.LENGTH_LONG).show();
+            Toast.makeText(getApplicationContext(), (String.format("USD %.2f", convertResult)), Toast.LENGTH_LONG).show();
+        }
     }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
